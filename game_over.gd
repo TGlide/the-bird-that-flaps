7265be41de94257b771aef182ec5de74561239cb
Thomas G. Lopes <26071571+TGlide@users.noreply.github.com>
2025-08-06 00:08:18 +0100

    save best score locally

--- game_over.gd
+++ game_over.gd
@@ -4,24 +4,48 @@ class_name GameOver
 @onready var go_text: TextureRect = $GOText
 @onready var animation: AnimationPlayer = $Animation
 @onready var score_label: Label = $Box/ScoreContainer/ScoreLabel
+@onready var best_label: Label = $Box/BestContainer/BestLabel
 
 const SCORE_ANIMATION_DURATION = 0.5
 
 var score: int
+var best: int
+
+var save_path = "user://score.save"
+
+func _ready():
+	load_score()
+
+func save_score():
+	var file = FileAccess.open(save_path, FileAccess.WRITE)
+	file.store_var(best)
+	print("saved", best)
+
+func load_score():
+	if FileAccess.file_exists(save_path):
+		print("file found")
+		var file = FileAccess.open(save_path, FileAccess.READ)
+		best = file.get_var()
+	else:
+		print("file not found")
+		best = 0
 
 func show_gameover(final_score: int) -> void:
+
+	if final_score > best:
+		best = final_score
+		save_score()
+
 	await get_tree().create_timer(0.5).timeout
 	animation.play("show")
 	show()
 	score = final_score
+	best_label.text = str(best)
 
 func _on_animation_animation_finished(anim_name: StringName) -> void:
-	print("animation finished", anim_name)
-
 	var s = 0
-	var timeout = SCORE_ANIMATION_DURATION / (score + 500.0)
-	print("timeout ", timeout)
-	while s < score + 500:
+	var timeout = SCORE_ANIMATION_DURATION / (score)
+	while s < score:
 		await get_tree().create_timer(timeout).timeout
 		s += 1
 		score_label.text = str(s)
